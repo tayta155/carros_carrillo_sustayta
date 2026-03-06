6460c4b3b0e839e054d1673f5008b4e2e533b595
mi-ga <sustayta_15@hotmail.com>
2026-03-06 13:39:08 -0600

    nombre de los controladores

--- backend/src/main/java/com/carros/backend/model/Vehiculo.java
+++ backend/src/main/java/com/carros/backend/model/Vehiculo.java
@@ -1,10 +1,27 @@
 package com.carros.backend.model;
 
-import jakarta.persistence.Entity;
+import jakarta.persistence.*;
+import lombok.*;
 
 @Entity
+@Table(name = "Vehiculos")
+
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+@Builder
 
 public class Vehiculo {
 
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    private String marca;
+
+    private String modelo;
+
+    private Integer anio;
 
 }
