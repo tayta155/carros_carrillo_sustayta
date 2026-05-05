cfee8664f5b378ac8bde8d5ca2676ddae0b057a2
tayta155 <sustayta_15@hotmail.com>
2026-05-05 17:59:27 -0600

    trabajo

--- frontend/src/pages/Login.jsx
+++ frontend/src/pages/Login.jsx
@@ -2,14 +2,14 @@ function Login() {
     return (
 
         // Fondo gris claro — ocupa toda la pantalla
-        <div className="min-h-screen bg-gray-50 flex items-center justify-center p-4">
+        <div className="min-h-screen bg-gray-200 flex items-center justify-center p-4">
              {/* Card centrada — máximo 400px de ancho */}
             <div className="w-full max-w-sm bg-white rounded-2x1 shadow-sm border border-gray-100 p-8 " >
 
                 <div className="mb-8">
                     <div className="w-10 h-10 bg-blue-600 rounded-xl mb-6" />
                     <h1 className="text-2x1 font-semibold text-gray-900"> Bienvenido!!! </h1>
-                    <p className="text-sm text-gray-500 mt-1"> Inicia sesión en tu cuenta 1234!!!! </p>
+                    <p className="text-sm text-gray-500 mt-1"> Inicia sesión en tu cuenta </p>
                 </div>
 
             {/* Formulario vacío por ahora */}
