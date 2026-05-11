4f1f48ecd72465ae5e3a1b4c90dbc99a174005e2
tayta155 <sustayta_15@hotmail.com>
2026-05-11 11:40:09 -0600

    frontend login

--- frontend/src/pages/Login.jsx
+++ frontend/src/pages/Login.jsx
@@ -2,23 +2,43 @@ function Login() {
     return (
 
         // Fondo gris claro — ocupa toda la pantalla
-        <div className="min-h-screen bg-gray-200 flex items-center justify-center p-4">
+        <div className="min-h-screen bg-gradient-to-r from-slate-900 to-slate-700 flex items-center justify-center p-4"> 
              {/* Card centrada — máximo 400px de ancho */}
-            <div className="w-full max-w-sm bg-white rounded-2xl shadow-sm border border-gray-100 p-8 relative before:absolute before:inset-0 before:rounded-2xl before:bg-gradient-to-r before:from-blue-500 before:via-purple-500 before:to-pink-500 before:blur-md before:-z-10">
+            <div className="w-full max-w-sm bg-white rounded-2xl p-8 border border border-cyan-950 shadow-[0_0_20px_rgba(125,211,252,0.7),0_0_80px_rgba(56,189,248,0.35)]">
 
-                <div className="mb-8">
-                    <div className="w-10 h-10 bg-blue-600 rounded-xl mb-6" />
-                    <h1 className="text-2x1 font-semibold text-gray-900"> Bienvenido!!! </h1>
-                    <p className="text-sm text-gray-500 mt-1"> Inicia sesión en tu cuenta andale mi perro jjjjdd </p>
+                <div className="h-24 mb-8 flex flex-col items-center justify-center">
+                    <h1 className="text-center font-semibold text-gray-900"> Bienvenido!!! </h1>
+                    <p className="text-sm text-gray-500 mt-1"> Inicia sesión en tu cuenta </p>
                 </div>
 
             {/* Formulario vacío por ahora */}
             <div className="space-y-4">
                 {/* Aquí van los inputs — Paso 2 */}
-                <div className="h-11 bg-gray-100 rounded-lg animate-pulse" />
-                <div className="h-11 bg-gray-100 rounded-lg animate-pulse" />
-                <div className="h-11 bg-blue-600 rounded-lg animate-pulse" />
+                <div>
+                    <p className="text-sm text-gray-500">Nombre de usuario</p>
+                    <div className="h-11 bg-gray-100 rounded-lg animate-pulse" >
+                    </div>
+                </div>
+                <div>
+                    <p className="text-sm text-gray-500">Contraseña</p>
+                    <div className="h-11 bg-gray-100 rounded-lg animate-pulse" />
+                </div>
+                <div>
+                    <div className="h-11 bg-blue-600 rounded-lg animate-pulse" >
+                        <button className="w-full h-full text-white font-medium" disabled>
+                            Iniciar sesión
+                        </button>
+                    </div>
+                </div>
             </div>
+
+            {/* Espacio entre el formulario y el footer */}
+            <div className="mb-6" >
+                <div>
+                    <p className="text-sm text-gray-500">contraseña olvidada?</p>
+                </div>
+                
+            </div> 
 
             {/* Footer */}
             <p className="text-center text-sm text-gray-500 mt-6">
